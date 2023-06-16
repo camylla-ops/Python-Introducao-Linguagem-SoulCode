dbe01d1b7d96e9f17b938c2c19e2ed0748f42b49
camylla <camylla.oliveiraps@gmail.com>
2023-06-15 22:31:13 -0300

    first commit

---
+++ Prática Proposta - Aula 01.py
@@ -0,0 +1,27 @@
+## Prática Proposta - Aula 01
+
+'''
+Como sugestão de prática, indicamos você a criar um novo script python seguindo as orientações a seguir:
+Crie quatro variáveis, sendo elas com nomes de animais, quantidade de animais, peso dos animais e se eles são vacinados ou não. 
+Comente seu código usando comentários longos e curtos.
+
+'''
+animal1 = 'cachorro'
+animal2 = 'Gato' 
+animal3 = 'Porco' 
+animal4 = 'hamster'
+
+quantidade_animais = 4
+
+pesoanimal1 = 10.5
+pesoanimal2 = 5
+pesoanimal3 = 20.4
+pesoanimal4 = 0.5
+
+#vacinação
+
+situaçao_animal1 = True     
+situaçao_animal2 = True
+situaçao_animal3 = False
+situaçao_animal4 = True
+
