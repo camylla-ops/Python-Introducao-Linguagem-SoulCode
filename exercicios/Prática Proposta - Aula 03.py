fe32761ec46c03e39716a38abf906760cac4667e
Mila <57633650+camylla-ops@users.noreply.github.com>
2023-06-17 15:31:56 -0300

    Create Prática Proposta - Aula 03.py

---
+++ exercicios/Prática Proposta - Aula 03.py
@@ -0,0 +1,18 @@
+'''
+Como sugestão de prática, indicamos você a criar um novo script python seguindo as orientações a seguir:
+
+Crie uma variável número e atribua o valor 15 a ela.
+Crie um comando de condição para verificar se ela é == a 15, 
+Menor que 15, maior que 15.
+
+Informar através de print a informação para o usuário.
+'''
+
+numero = 15
+
+if numero == 15:
+    print("O número é igual a 15.")
+elif numero <= 15:
+    print("O número é menor que 15.")
+else:
+    print("O número é maior que 15.")
