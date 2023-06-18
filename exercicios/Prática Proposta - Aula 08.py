ceac227db3036f05944dbaf8e7eadff485fdd12e
Mila <57633650+camylla-ops@users.noreply.github.com>
2023-06-18 13:44:30 -0300

    Create Prática Proposta - Aula 08.py

---
+++ exercicios/Prática Proposta - Aula 08.py
@@ -0,0 +1,10 @@
+'''
+Como sugestão de prática, indicamos você a criar um novo script python seguindo as orientações a seguir:
+
+Crie um dicionário contendo dias da semana sendo dia1, dia2, dia3... as chaves e o dia seu valor. Ex: “dia1”: “domingo”.
+Remova dois dos últimos dias da semana.
+Remova segunda-feira pela sua chave.
+Imprima chaves e valores do dicionário.
+Imprima o dicionário final.
+'''
+
