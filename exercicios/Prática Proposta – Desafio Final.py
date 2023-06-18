96ace7b44117ca41e62e69e26e4f5996b71281c7
Mila <57633650+camylla-ops@users.noreply.github.com>
2023-06-18 13:46:16 -0300

    Create Prática Proposta – Desafio Final.py

---
+++ exercicios/Prática Proposta – Desafio Final.py
@@ -0,0 +1,9 @@
+'''
+Como sugestão de prática, indicamos você a criar um novo script python seguindo as orientações a seguir:
+
+Deve se criar 5 funções diferentes, sendo elas somar, subtrair, dividir, multiplicar e listar.
+Deve se criar um MENU com if else sendo que o usuário escolha qual função deve acessar. 
+Ao ser escolhido a função, o usuário é direcionado a ela e insere os parâmetros que desejam realizar as operações.
+As funções matemáticas devem receber os números para realizar a operação, realizar o cálculo e imprimir o resultado para o usuário.
+Na função listar, deve-se criar uma lista de vantagens do python e imprimir para o usuário.
+'''
