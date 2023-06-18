ec864a434a9823f5dad3bfb3ed789318058ef3d1
Mila <57633650+camylla-ops@users.noreply.github.com>
2023-06-18 13:40:05 -0300

    Create Prática Proposta - Aula 06.py

---
+++ exercicios/Prática Proposta - Aula 06.py
@@ -0,0 +1,17 @@
+'''
+Como sugestão de prática, indicamos você a criar um novo script python seguindo as orientações a seguir:
+
+Crie uma lista abaixo com a seguinte palavra:
+“PYTHON”
+
+A lista deve conter as letras separadas e em maiúsculas.
+Crie uma nova lista somente com as três últimas letras da palavra “PYTHON”.
+'''
+
+lista = ['P', 'Y', 'T', 'H', 'O', 'N']
+
+lista_nova = lista [3:6]
+
+print(lista_nova)
+
+
