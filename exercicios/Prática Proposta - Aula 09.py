f7d8516f020cdb18f364bdaffcb73f692a42ffdb
Mila <57633650+camylla-ops@users.noreply.github.com>
2023-06-18 13:45:31 -0300

    Create Prática Proposta - Aula 09.py

---
+++ exercicios/Prática Proposta - Aula 09.py
@@ -0,0 +1,6 @@
+'''
+Como sugestão de prática, indicamos você a criar um novo script python seguindo as orientações a seguir:
+
+Crie uma função que receba dois parâmetros e realize sua soma, subtração, adição e multiplicação.
+Informe esses resultados através de um print ao usuário dentro da função.
+'''
