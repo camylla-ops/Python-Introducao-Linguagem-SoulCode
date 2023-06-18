80ecf878bde32034adf3d7b2047009c237ef33f2
Mila <57633650+camylla-ops@users.noreply.github.com>
2023-06-18 13:17:03 -0300

    Create Prática Proposta - Aula 04.py

---
+++ exercicios/Prática Proposta - Aula 04.py
@@ -0,0 +1,8 @@
+'''
+Como sugestão de prática, indicamos você a criar um novo script python seguindo as orientações a seguir:
+
+Crie um laço com for in range que imprima os múltiplos de 3 no intervalo entre 0 e 15.
+'''
+
+for i in range(0, 18, 3):
+    print(i)
